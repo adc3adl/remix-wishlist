0b54789049e8417c4990c701031318991505dc77
adc3adl <klovovlla@gmail.com>
2025-06-11 15:22:18 +0300

    variant price  refresh6

--- public/wishlist.js
+++ public/wishlist.js
@@ -214,7 +214,7 @@ function showLoginModal(targetElement) {
       title.textContent = product.title;
 
       const price = document.createElement("span");
-      price.textContent = `${p.price} ${p.currency || "UAH"}`;
+      price.textContent = `${product.price} ${product.currency || "UAH"}`;
 
       const addToCartBtn = document.createElement("button");
       addToCartBtn.textContent = "🛒 Add to cart";
